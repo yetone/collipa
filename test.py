3552cf7000656e55bc913abb9b8d72dbd9f5c869
yetone <i@yetone.net>
2013-09-17 23:10:29 +0000

    fix bug: fix first send message bug

--- test.py
+++ test.py
@@ -9,9 +9,19 @@ config = config.rec()
 def do_sth():
     users = select(rv for rv in m.User if rv.role == 'unverify')
     message_boxes = m.MessageBox.select()
+    messages = m.Message.select()
 
     for user in users:
         print user.name, user.nickname, user.email
+    """
+    for ms in messages:
+        print ms.id
+        try:
+            ms.receiver_id = ms.message_box1.receiver_id
+            commit()
+        except:
+            print "error"
+    """
     for mb in message_boxes:
         print mb.id
         mb.delete()
