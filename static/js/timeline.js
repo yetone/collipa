4b86411e48e8c23bb075ca3cad7d760b8c54fd7b
yetone <i@yetone.net>
2014-02-24 21:39:29 +0000

    fix bug: fix timeline bug

--- static/js/timeline.js
+++ static/js/timeline.js
@@ -161,49 +161,4 @@ $(function() {
       }
     });
   });
-  $D.on('click', '.tweet-img-content', function() {
-    var $this = $(this),
-        $covers = $this.find('.img-cover'),
-        $dns = $this.find('.img-cover.dn'),
-        $ul = $this.prev('.thumbs').find('ul'),
-        $thumbs = $ul.find('li'),
-        $area = $this.parents('.tweet-img-area');
-    $ul.width(($thumbs.outerWidth() + 10) * $thumbs.length);
-    if ($area.hasClass('close')) {
-      $area.removeClass('close').addClass('open');
-      $thumbs.removeClass('cur');
-      $thumbs.eq(0).addClass('cur');
-      $covers.hide();
-      $covers.eq(0).show();
-    } else {
-      $area.removeClass('open').addClass('close');
-      $covers.show();
-      $dns.hide();
-    }
-  });
-  $D.on('click', '.thumbs li', function() {
-    var $this = $(this),
-        $area = $this.parents('.tweet-img-area'),
-        $ul = $this.parent('ul'),
-        $thumbs = $ul.find('li'),
-        $covers = $area.find('.img-cover'),
-        idx = $thumbs.index($this[0]);
-    $covers.hide();
-    $covers.eq(idx).show();
-    $thumbs.removeClass('cur');
-    $this.addClass('cur');
-  });
-  $D.on('mousemove', '.thumbs', function(e) {
-    var $this = $(this),
-        $ul = $this.find('ul'),
-        pos = e.pageX - $this.offset().left,
-        posP = pos / $this.width(),
-        listP = $ul.width() * posP,
-        offset = pos - listP;
-    if ($ul.width() > $this.width()) {
-      $ul.css({
-        'left': offset
-      });
-    }
-  });
 });
